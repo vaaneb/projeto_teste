fc7ceb64ada7525d250c603ff21cb257ed994050
Vanessa Batista <vanne_b@outlook.com>
2022-02-08 17:31:07 -0300

    Configurando a página inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
+  root "testes#index"
   resources :testes
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
 end
